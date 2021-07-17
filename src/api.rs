f81b39d99c91234f7b040585ac30e9a45cf63d59
nilSinger <niilz@outlook.de>
2021-07-17 10:27:05 +0200

    Added information to where the client-id can be obtained

--- src/api.rs
+++ src/api.rs
@@ -6,7 +6,7 @@ const OPENID_PROVIDER: &'static str = "https://accounts.google.com/o/oauth2/v2/a
 
 #[get("/start")]
 pub async fn start_demo() -> (ContentType, String) {
-    let client_id = env::var("CLIENT_ID").expect("Please define client ID as env-var CLIENT_ID");
+    let client_id = env::var("CLIENT_ID").expect("Please define client ID (get it from google-app-credentials-dashboard) as env-var CLIENT_ID");
     println!("starting the dance");
     let req = Request::new(&client_id);
     let res = reqwest::get(req.to_url(OPENID_PROVIDER.to_string()))
