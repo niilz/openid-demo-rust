4b08f9a3c5add40030fc61f70042d891c2ef4b77
nilSinger <niilz@outlook.de>
2021-11-03 22:48:35 +0100

    Renamed CurrentState initialize method
    Attempt was to get rid of the Mutex. But I think that
    is not possible very easily

--- src/api.rs
+++ src/api.rs
@@ -7,21 +7,23 @@ use std::sync::Mutex;
 const OPENID_PROVIDER: &'static str = "https://accounts.google.com/o/oauth2/v2/auth";
 
 #[derive(Default, Debug)]
-pub struct CurrentState(pub Mutex<String>);
+pub struct CurrentState(Mutex<String>);
 impl CurrentState {
-    fn set_state(&self, new_state: String) {
+    pub fn init_for_req(&self, new_state: String) {
         if let Ok(mut state) = self.0.lock() {
             *state = new_state;
+        } else {
+            eprint!("Could not set new sate for code-flow-start-request");
         }
     }
 }
 
 #[get("/start")]
-pub async fn start_demo(current_state: &State<CurrentState>) -> Redirect {
+pub async fn start_demo(request_state: &State<CurrentState>) -> Redirect {
     let client_id = env::var("CLIENT_ID").expect("Please define client ID (get it from google-app-credentials-dashboard) as env-var CLIENT_ID");
     let req = Request::new(&client_id);
     let state = req.get_state().to_string();
-    current_state.set_state(state);
+    request_state.init_for_req(state);
     let url = req.to_url(OPENID_PROVIDER.to_string());
     Redirect::to(url)
 }
