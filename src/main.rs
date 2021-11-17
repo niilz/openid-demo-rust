77959468806c4d0b4d112cc120dda89896abd643
nilSinger <niilz@outlook.de>
2021-11-17 20:25:59 +0100

    Add the InMermoryRepo as State inside a Mutex

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ extern crate rocket;
 
 use openid::api::*;
 use openid::credentials::Credentials;
+use openid::service::user::InMemoryUserRepository;
+use std::sync::Mutex;
 
 #[launch]
 fn app() -> _ {
     let credentials = Credentials::init();
 
     rocket::build()
+        .manage(Mutex::new(InMemoryUserRepository::default()))
         .manage(CurrentState::default())
         .manage(credentials)
         .mount("/", routes![login, handle_success])
